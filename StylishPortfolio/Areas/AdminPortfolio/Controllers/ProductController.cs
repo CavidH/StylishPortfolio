aa486cdc10b1c004622e9a00f600303f91301b4b
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-28 00:46:59 +0400

    iis eror jssss

--- StylishPortfolio/Areas/AdminPortfolio/Controllers/ProductController.cs
+++ StylishPortfolio/Areas/AdminPortfolio/Controllers/ProductController.cs
@@ -28,6 +28,14 @@ namespace StylishPortfolio.Areas.AdminPortfolio.Controllers
             var projects = await _context.projects.Where(p => p.IsDeleted == false).ToListAsync();
             return View(projects);
         }
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var project = await _context.projects.FindAsync(Id);
+            project.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
         public async Task<IActionResult> Create()
         {
 
@@ -36,28 +44,58 @@ namespace StylishPortfolio.Areas.AdminPortfolio.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CreateVM createVm)
         {
-             
-            if (ModelState.IsValid)  
-            {  
-                string  FileName = ProcessUploadedFile(createVm.ImageFile);
-                var project = new Project { 
-                Name=createVm.Name,
-                Summary=createVm.Summary,   
-                Image= FileName
+
+            if (ModelState.IsValid)
+            {
+                string FileName = ProcessUploadedFile(createVm.ImageFile);
+                var project = new Project
+                {
+                    Name = createVm.Name,
+                    Summary = createVm.Summary,
+                    Image = FileName
                 };
                 await _context.AddAsync(project);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
-            }  
-            return View(createVm);  
+            }
+            return View(createVm);
         }
+        public async Task<IActionResult> Update(int Id)
+        {
+            var project = await _context.projects.FindAsync(Id);
+            if (project == null) return NotFound();
+            var UpdateVm= new UpdateVM
+            {
+                Name=project.Name,
+                Summary=project.Summary
+            };
+            return View(UpdateVm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(int Id, UpdateVM updateVM)
+        {
+            var project = await _context.projects.FindAsync(Id);
+            if (ModelState.IsValid)
+            {
+                project.Name = updateVM.Name;
+                project.Summary = updateVM.Summary;
+                if (updateVM.ImageFile!=null)
+                {
+                    string imgname=ProcessUploadedFile(updateVM.ImageFile);
+                    project.Image = imgname;
+                }
+               
+            }
+            return View(updateVM);
+        }
+
         private string ProcessUploadedFile(IFormFile File)
         {
             string uniqueFileName = null;
 
-            if (File  != null)
+            if (File != null)
             {
-                string uploadsFolder = Path.Combine(_env.WebRootPath, "assets","img");
+                string uploadsFolder = Path.Combine(_env.WebRootPath, "assets", "img");
                 uniqueFileName = Guid.NewGuid().ToString() + "_" + File.FileName;
                 string filePath = Path.Combine(uploadsFolder, uniqueFileName);
                 using (var fileStream = new FileStream(filePath, FileMode.Create))
@@ -68,6 +106,7 @@ namespace StylishPortfolio.Areas.AdminPortfolio.Controllers
 
             return uniqueFileName;
         }
+
 
     }
 }
---
+++ StylishPortfolio/Areas/AdminPortfolio/ViewModels/UpdateVM.cs
@@ -0,0 +1,9 @@
+﻿namespace StylishPortfolio.Areas.AdminPortfolio.ViewModels
+{
+    public class UpdateVM
+    {
+        public string Name { get; set; }
+        public string Summary { get; set; }
+        public IFormFile ImageFile { get; set; }s
+    }
+}
