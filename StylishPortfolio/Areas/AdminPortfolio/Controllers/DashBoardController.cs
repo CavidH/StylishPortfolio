39883e2078cbd82a16aa4b5c96e7ede78cd70dd5
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-27 22:11:53 +0400

    start Adminpanel :rocket:

---
+++ StylishPortfolio/Areas/AdminPortfolio/Controllers/DashBoardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StylishPortfolio.Areas.AdminPortfolio.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        [Area("AdminPortfolio")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- StylishPortfolio/Startup.cs
+++ StylishPortfolio/Startup.cs
@@ -50,6 +50,10 @@ namespace StylishPortfolio
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "Admin",
+                    pattern: "{area:exists}/{controller=DashBoard}/{action=Index}/{id?}"
+             );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
